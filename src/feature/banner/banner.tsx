933371f174322d0d20a055d448bdfe232b7e851b
Yeonu-Kim <ywk0524@snu.ac.kr>
2025-04-29 17:11:32 +0900

    💄 글자 크기 수정

--- src/feature/banner/banner.tsx
+++ src/feature/banner/banner.tsx
@@ -3,7 +3,7 @@ import { VerticalCarousel } from '@/feature/banner/vertical-carousel';
 export const Banner = () => {
   return (
     <div className="flex h-full w-full">
-      <div className="flex items-center gap-2 py-4 text-sm font-bold sm:text-lg">
+      <div className="xs:text-lg flex items-center gap-2 py-4 text-sm font-bold">
         <h3>제 2의</h3>
         <VerticalCarousel />
         <h3>에서 근무하세요</h3>
