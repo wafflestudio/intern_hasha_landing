e41501854ae517a01c4cb3817e3a6730cbfff94e
Yeonu-Kim <ywk0524@snu.ac.kr>
2025-04-29 11:43:31 +0900

    ✨ shadcn 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { NavigationBar } from './components/common/navigation-bar';
-import { Banner } from './feature/Banner';
-import { CoffeechatIntroduction } from './feature/coffeechat-introduction';
-import { Contact } from './feature/contact';
-import { Hero } from './feature/hero';
-import { VcIntroduction } from './feature/vc-introduction/VcIntroduction';
+import { NavigationBar } from '@/components/common/navigation-bar';
+import { Banner } from '@/feature/banner';
+import { CoffeechatIntroduction } from '@/feature/coffeechat-introduction';
+import { Contact } from '@/feature/contact';
+import { Hero } from '@/feature/hero';
+import { VcIntroduction } from '@/feature/vc-introduction/VcIntroduction';
 
 function App() {
   return (
