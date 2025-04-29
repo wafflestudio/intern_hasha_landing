adbcdd872ab26c478eaef92d89d29a9b3292ffd4
Yeonu Kim <88171936+Yeonu-Kim@users.noreply.github.com>
2025-04-29 11:50:51 +0900

    ➕ tailwind 및 shadcn 세팅 (#3)
    - knip에서 tailwindcss를 사용하지 않는다고 나타나 일단 knip을 제거함

--- src/feature/hero/Hero.tsx
+++ src/feature/hero/Hero.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/common/button';
+
 export const Hero = () => {
   return (
-    <div>
+    <div className="flex">
       <h1>이번 여름 인턴, 5분만에 구하세요!</h1>
+      <Button>클릭</Button>
     </div>
   );
 };
