b5569ec01547363cbef614f029a59acfde67d151
Yeonu-Kim <ywk0524@snu.ac.kr>
2025-04-30 20:32:55 +0900

    ✨ 로고 클릭 시 최상단으로 이동

--- src/components/common/navigation-bar.tsx
+++ src/components/common/navigation-bar.tsx
@@ -1,11 +1,19 @@
 import { ASSET_SRC } from '@/entities/assets';
 
 export const NavigationBar = () => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
   return (
     <div className="t-0 l-0 fixed z-10 flex h-15 w-screen items-center justify-center bg-linear-(--white-gradient)">
       <img
         src={ASSET_SRC.LOGO}
         className="w-23"
+        onClick={scrollToTop}
       />
     </div>
   );
