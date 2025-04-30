d6d56938cbcfcca668599df3d2bd8ba6c3735e1f
Yeonu Kim <88171936+Yeonu-Kim@users.noreply.github.com>
2025-04-30 20:34:07 +0900

    ✨ 로고 클릭 시 최상단으로 이동 (#17)

--- src/entities/assets.ts
+++ src/entities/assets.ts
@@ -1,6 +1,6 @@
 export const ASSET_SRC = {
   JOB_CARD: '/mockup/job_card.png',
   COFFECHAT_PAGE: '/mockup/resume.png',
-  LOGO: '/logo/logo.png',
+  LOGO: '/logo/logo.svg',
   ARROW: '/icon/arrow.svg',
 };
--- src/feature/banner/banner.tsx
+++ src/feature/banner/banner.tsx
@@ -3,7 +3,7 @@ import { VerticalCarousel } from '@/feature/banner/vertical-carousel';
 export const Banner = () => {
   return (
     <div className="flex h-full w-full">
-      <div className="xs:text-[1rem] flex items-center gap-2 py-4 text-sm font-bold">
+      <div className="xs:text-[1rem] flex items-center gap-2 py-4 text-sm font-bold md:gap-4 md:text-lg">
         <h3>제 2의</h3>
         <VerticalCarousel />
         <h3>에서 근무하세요</h3>
